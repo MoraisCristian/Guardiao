a76dd35b86b6ea719314fffecdb5ac1b526e9bf4
Pasch0 <pasch0.s3c@gmail.com>
2025-03-20 10:28:36 -0300

    integrado ossec com zincsearch

--- zincsearch/ossec_to_zinc.sh
+++ zincsearch/ossec_to_zinc.sh
@@ -3,6 +3,10 @@
 # Install required dependencies
 pip install requests
 
+# Install curl
+apt-get update
+apt-get install -y curl
+
 # Wait for ZincSearch to be ready
 echo "Waiting for ZincSearch to be ready..."
 until $(curl --output /dev/null --silent --head --fail http://zincsearch:4080); do
