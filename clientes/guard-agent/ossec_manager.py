1160a01b935f69fae40ad2058f1387dc8ab7db8d
Pasch0 <pasch0.s3c@gmail.com>
2025-03-30 15:08:06 -0300

    pequenos ajustes

--- clientes/guard-agent/ossec_manager.py
+++ clientes/guard-agent/ossec_manager.py
@@ -246,7 +246,7 @@ def instalar_ossec():
         # Instala dependências necessárias
         if distro == "debian":
             dependencies = ["build-essential",  # includes make, gcc, and other build tools
-                          "libevent-dev",    # changed from libevent-dev
+                          "libevent-2.1-7t64",    # changed from libevent-dev
                           "libpcre2-dev", 
                           "libssl-dev",
                           "zlib1g-dev",
